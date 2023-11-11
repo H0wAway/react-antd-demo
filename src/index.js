066eb01219331cb8632511a2f4b228c0bb004b81
H0wAway <scatterchu@gmail.com>
2023-11-12 07:55:49 +0800

    feat: 简单功能实现

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './App.css';
-import 'antd/dist/antd'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import DefaultLayout from "./components/framework/layout";
+import Detail from "./pages/menus/Deatil";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// React18+router6标准写法
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <BrowserRouter>
+      <Routes>
+        {/* 根目录 */}
+        <Route path="/" element={<App />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route
+          path="/admin"
+          element={
+            <DefaultLayout>
+              <App />
+            </DefaultLayout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
